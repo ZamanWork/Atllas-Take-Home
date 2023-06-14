88069498799c34ec96a3329ecaf09ac51ee7b19f
Zaman <dev.zaman.work@gmail.com>
2023-06-14 20:11:57 +0500

    Initial Commit: Header and Footer basic design structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,7 @@
 import type { FC } from "react";
+import Header from '../../Layouts/Header/Header'
+import Footer from '../../Layouts/Footer/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 import Agents from "../Agents/Agents";
@@ -6,7 +9,9 @@ import Agents from "../Agents/Agents";
 const App: FC = () => {
   return (
     <div className="app">
+      <Header title="Agents App" />
       <Agents />
+      <Footer title="Agents App Footer" />
     </div>
   );
 };
