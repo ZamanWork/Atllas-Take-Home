322c342c0e26affb34063cfe900c293e82481dc5
Zaman <dev.zaman.work@gmail.com>
2023-06-15 16:26:03 +0500

    Added Interface for Agent Reviews

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import type { FC } from "react";
-import Header from '../../Layouts/Header/Header'
-import Footer from '../../Layouts/Footer/Footer'
+import Header from 'Layouts/Header/Header'
+import Footer from 'Layouts/Footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-import Agents from "../Agents/Agents";
+import Agents from "../Agents";
 
 const App: FC = () => {
   return (
